c62bdad6c6b6b1ec3ca18387f052e4c0e5182499
Mohammed.Abdelrahman <mohammed.abdelrahman@ascendlearning.com>
2019-07-15 18:11:17 -0500

    init push request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'iframe-scroll';
+  showDrawer = false;
+
+  ngOnInit() {
+    const userAgent = navigator.userAgent;
+    const scrollWrapper = document.getElementsByClassName('matSidenavContent');
+
+    if (/iPad/i.test(userAgent)) {
+      scrollWrapper[0].classList.add('ipad-scrollbar-enabler');
+    }
+
+  }
 }
---
+++ src/app/core/mat/mat.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class MatModule { }
